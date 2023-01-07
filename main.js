9616734e2e63c65c4c733a086e57f6b4754a53ac
Matias Lopez Cortese <matais.lopez.89@hotmail.com>
2023-01-07 13:14:40 -0300

    primera entrega js

---
+++ main.js
@@ -0,0 +1,25 @@
+function plazoFijoArg(capital, dias){
+    return (capital * dias * 75)/36500
+}
+
+function plazoFijoDol(capital, dias){
+    return (capital * dias * 0.3)/36500
+}
+
+let seleccion = prompt ("Seleccionar tipo de plazo fijo \n 1-Plazo fijo en pesos \n 2-Plazo fijo en dolares")
+let capital = prompt("Ingrese capital")
+let dias = prompt("ingrese plazo")
+
+switch(seleccion){
+    case "1":
+        alert(plazoFijoArg(capital, dias));
+        console.log(resultado);
+        break;
+    
+    case "2":
+        alert(plazoFijoDol(capital, dias));
+        console.log(resultado);
+        break;
+    default:
+        break;
+}
