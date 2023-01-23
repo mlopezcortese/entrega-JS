c1259735183aed5a79fbf0666beb465048ddd6a6
Matias Lopez Cortese <matais.lopez.89@hotmail.com>
2023-01-23 00:44:07 -0300

    2da entrega

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-
+/*
 let clave = 'river';
 //funcion
 function login() {
@@ -50,3 +50,68 @@ if (login()) {
             break;
     }
 }
+*/
+
+//declare la clase paquete para archivar los distintos componentes, a eso le cree un arrai con las distintas partes del paquete, para esto me base en la actividad de la clase 7 del workbook y le agregue y cambie algunas cosas
+
+class paquete{
+    constructor (nombre,costo,beneficios,categoria){
+        this.nombre = nombre;
+        this.costo = parseFloat(costo);
+        this.beneficios = beneficios;
+        this.categoria = parseInt (categoria);
+    }
+}
+ 
+
+const paquetes = [];
+paquetes.push(new paquete("IMPULSO",1000,["C.Ahorro", "Tc Visa"],1));
+paquetes.push(new paquete("CRECIMIENTO",2000,["C.Ahorro","Tc Visa","Tc Master"],2))
+paquetes.push(new paquete("EVOLUCION",3000,["C.Ahorro","Tc Visa","Tc Master","C.Ahorro Dol",],2));
+paquetes.push(new paquete("LOGROS",4000,["C.Ahorro","Tc Visa","Tc Master","C.Ahorro Dol", "cta cte"],4));
+
+localStorage.setItem("paquetes", JSON.stringify(paquetes));
+
+
+
+
+let guardadasLS = localStorage.getItem("paquetes");
+if (guardadasLS != null) {
+    let seleccionPaq = JSON.parse(guardadasLS);
+    let venta = "Seleccionar Paquete \n";
+    for (let i = 0; i< seleccionPaq.length; i++) {
+        venta += i + " = " + seleccionPaq[i].nombre + " " + seleccionPaq[i].beneficios + "$" + seleccionPaq[i].costo + '\n';
+    }
+    alert(venta);
+    let eleccion = parseInt(prompt("Seleccione Paquete"));
+    if ((eleccion >= 0) && (eleccion < seleccionPaq.length)) {
+        alert(" Paquete Seleccionado " + seleccionPaq[eleccion].nombre + " Contiene: " + seleccionPaq[eleccion].beneficios)
+      
+    } else{
+        alert ( "Seleccione una de las opciones disponibles")
+    }
+}
+
+//aca agrego un descuento del 50% para primeros clientes.
+let paqGuardados = localStorage.getItem("paquetes");
+if (paqGuardados != null) {
+    let seleccionPaq = JSON.parse(paqGuardados);
+    seleccionPaq.forEach(paquete => { paquete.costo -= (paquete.costo * 50)/100});
+    
+    localStorage.setItem("paquetes", JSON.stringify(seleccionPaq));
+}
+
+//agregue el elemmento POP para quitar el ultimo paquete del array
+paquetes.pop()
+console.log(paquetes)
+
+
+//agregue un filtro para encontrar paquetes con un costo menor a $3000
+const resultado = paquetes.filter((el) =>el.costo <3000)
+console.log(resultado)
+
+/*/
+localStorage.clear()
+*/
+
+
